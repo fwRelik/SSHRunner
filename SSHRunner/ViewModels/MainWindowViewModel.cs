87062e578144ec043c2538a11b4264b6d6b721d9
fwRelik <relik.fw@gmail.com>
2023-03-04 20:41:33 +0300

    Binding test

---
+++ SSHRunner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SSHRunner.ViewModels.Base;
+
+namespace SSHRunner.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window title
+
+        /// <summary>Window title</summary>
+        private string _title = "SSH Runner";
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+    }
+}
