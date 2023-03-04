4c2bf8779b8e9a7853db1185b36eb16ba4f1d855
fwRelik <relik.fw@gmail.com>
2023-03-04 20:52:54 +0300

    Added infrastructure

---
+++ SSHRunner/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using SSHRunner.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace SSHRunner.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
